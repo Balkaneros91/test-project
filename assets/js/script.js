3a99b77f0b6a5b123442d7ab1bf0803918f1f6b2
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2022-11-16 13:19:58 +0000

    Add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+
+
+/*
+let images = "./assets/images/dice";
 let images = ["dice1.png",
 "dice2.png",
 "dice3.png",
@@ -7,6 +11,7 @@ let images = ["dice1.png",
 let dice = document.querySelectorAll("img");
 
 /*console.log(dice);*/
+/*
 function roll() {
     dice.forEach(function(die) {
         die.classList.add("shake");
@@ -15,18 +20,22 @@ function roll() {
         dice.forEach(function(die) {
             die.classList.remove("shake");
         });
-        let dieOneValue = Math.floor(Math.random()*6 +1);
-        let dieTwoValue = Math.floor(Math.random()*6 +1);
+
+        // Generate a rand.nr 1-6
+        let dieOneValue = Math.floor(Math.random()*6) +1;
+        let dieTwoValue = Math.floor(Math.random()*6) +1;
         console.log(dieOneValue,dieTwoValue);
+        
         document.getElementById("die-1").setAttribute("src", images[dieOneValue]);
         document.getElementById("die-2").setAttribute("src", images[dieTwoValue]);
         document.getElementById("round-result").innerHTML = ( (dieOneValue) + (dieTwoValue) );
+        
     },
     1000
     );
 }
 roll();
-
+*/
 /*
 let images = ["dice-01.svg",
 "dice-02.svg",
@@ -57,3 +66,10 @@ function roll(){
 roll();
 
 */
+
+/*
+// Get the rand.nr dice1 - dice6
+let dieOneValue = Math.floor(Math.random()*6) +1;
+let firstDiceImage = "./assets/images/dice" + dieOneValue + ".png";
+*/
+
