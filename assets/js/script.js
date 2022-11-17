b209b3d09e45ef6dd2dbdd2acc08bb2a34272a9d
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2022-11-17 15:43:20 +0000

    Add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+let result = 0;
+let targNr = document.getElementById('targ-Nr').value;
+let chances = document.getElementById('chances').innerText;
+
 document.addEventListener('DOMContentLoaded', function () {
     let buttons = document.getElementsByTagName('button');
 
@@ -6,20 +10,92 @@ document.addEventListener('DOMContentLoaded', function () {
             if (this.getAttribute('data-type') === 'submit') {
                 /* alert('You roll the dice!'); */
                 rollDice();
+                checkOutcome();
             } else if (this.getAttribute('data-type') === 'reset') {
                 /* alert('GAME OVER - GAME RESET!'); */
-                resetGame();
+                window.location.reload(true);
             } else {
                 let targNr = this.getAttribute('data-choice');
                 /* console.log(targNr); */
                 /* alert(`You picked the target number ${targNr}!`); */
-                document.querySelector("#targ-Nr").innerHTML = `Your target number is ${targNr}! Let's play :)`
+                document.querySelector("#targ-Nr").innerHTML = `You choosen number  ${targNr}! Let's play :)`;
+                document.getElementById("roll").disabled = false;
             }
         });
     }
-    rollDice('6');
+    /*rollDice('6');*/
+
 });
 
+
+let dice = document.querySelectorAll('img');
+/* console.log(dice); */
+
+/**
+ * 
+ */
+function rollDice() {
+    dice.forEach(function (die) {
+        die.classList.add("shake");
+    });
+    setTimeout(function () {
+            dice.forEach(function (die) {
+                die.classList.remove("shake");
+            });
+
+            let dieOneValue = Math.floor(Math.random() * 6) + 1;
+            let firstDiceImage = "assets/images/dice" + dieOneValue + ".png";
+            let dieTwoValue = Math.floor(Math.random() * 6) + 1;
+            let secondDiceImage = "assets/images/dice" + dieTwoValue + ".png";
+            /*console.log(dieOneValue, dieTwoValue);*/
+
+            document.querySelector("#die1").setAttribute("src", firstDiceImage);
+            document.querySelector("#die2").setAttribute("src", secondDiceImage);
+            document.querySelector("#round-result").innerHTML = "Your roll is " + ((dieOneValue) + (dieTwoValue));
+            result = document.getElementById('round-result').value;
+        },
+        2000
+    );
+}
+
+/**
+ * Gets the outcome of the throw 
+ */
+function checkOutcome() {
+    let userThrow = parseInt(document.getElementById('round-result').value);
+    /*console.log('userThrow');*/
+    /*let calculateDice = checkOutcome();*/
+    let isMatch = userThrow === result;
+    let isNotMatch = userThrow !== result;
+
+    if (isMatch === targNr) {
+        alert(`You throw ${targNr}! Congrats it's a match, reset and play again! :D`);
+    } else if (isNotMatch !== targNr) {
+        alert(`It's not a match. You have ${chances - 1} left!`);
+        decrementChances();
+    } else {
+        (chances === result);
+        alert("You run out chances, reset and play again!");
+    }
+
+    /*rollDice(); */
+}
+
+/**
+ * Gets the total number of throws until the game ends
+ */
+function decrementChances() {
+
+    chances = parseInt(document.getElementById('chances').innerText);
+    document.getElementById('chances').innerText = --chances;
+
+    /*for (let i = 0; i <= 5; i--)*/
+}
+
+
+
+
+/*
 let images = ['dice1.png',
     'dice2.png',
     'dice3.png',
@@ -28,58 +104,6 @@ let images = ['dice1.png',
     'dice6.png'
 ];
 
-let dice = document.querySelectorAll('img');
-/* console.log(dice); */
-
-/**
- * The main game "loop", called when the script is first loaded
- * and after the user's answer has been processed
- */
-
-function rollDice(targNr) {
-    dice.forEach(function (die) {
-        die.classList.add("shake");
-    });
-    setTimeout(function () {
-            dice.forEach(function (die) {
-                die.classList.remove("shake");
-            });
-            let dieOneValue = Math.floor(Math.random() * 6);
-            let firstDiceImage = "assets/image/dice" + dieOneValue + ".png";
-            let dieTwoValue = Math.floor(Math.random() * 6);
-            let secondDiceImage = "assets/images/dice" + dieTwoValue + ".png";
-            /* console.log(dieOneValue, dieTwoValue); */
-            document.querySelector("#die1").setAttribute("src", images[dieOneValue]);
-            document.querySelector("#die2").setAttribute("src", images[dieTwoValue]);
-            document.querySelector("#round-result").innerHTML = "Your roll is " + ((dieOneValue + 1) + (dieTwoValue + 1));
-        },
-        1000
-    );
-}
-
-/**
- * Gets the outcome of the throw 
- */
-function checkOutcome() {
-    let userThrow = document.querySelector('#round-result').innerHTML.valueOf;
-    /* console.log('userThrow'); */
-
-}
-
-/**
- * Gets the total number of throws until the game ends
- */
-/*function decrementChances ()
-/**
- * Resets the game 
- */
-/*function restartGame ()
-
-
-
-
-
-/*
 let images = ["dice-01.svg",
 "dice-02.svg",
 "dice-03.svg",
@@ -107,7 +131,6 @@ function roll(){
     );
 }
 roll();
-
 */
 
 /*
