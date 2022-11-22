34674f15f11fec7b0c3fa893fd14a18278f22ff0
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2022-11-22 10:39:41 +0000

    Add new changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 let dice = document.querySelectorAll('img');
 
-
 document.addEventListener('DOMContentLoaded', function () {
     let buttons = document.getElementsByTagName('button');
 
@@ -13,18 +12,35 @@ document.addEventListener('DOMContentLoaded', function () {
             } else {
                 let targNr = this.getAttribute('data-choice');
                 document.getElementById("targ-Nr").innerHTML = targNr;
+
+                /*let num6 = document.getElementById('control-6');
+                let num7 = document.getElementById('control-7');
+                let num8 = document.getElementById('control-8');
+                let num9 = document.getElementById('control-9');
+
+                if (num6 === targNr) {
+                    num7 = 'disabled';
+                    num8 = 'disabled';
+                    num9 = 'disabled';
+                } else {
+                    'enable';
+                }
+
+                /*if (this.getAttribute('data-choice').value === ('targNr').textContent) {
+                    document.getElementsByClassName('btn').disabled = true;
+                } else {
+                    document.getElementsByClassName('btn').disabled = false;
+                }*/
                 document.getElementById("roll").disabled = false;
             }
         });
     }
 });
 
-
 function rollDice() {
     dice.forEach(function (die) {
         die.classList.add("shake");
     });
-
 
     let dieOneValue = Math.floor(Math.random() * 6) + 1;
     let firstDiceImage = "assets/images/dice" + dieOneValue + ".png";
@@ -47,26 +63,22 @@ function rollDice() {
 
 }
 
-
 function checkOutcome() {
 
     let result = parseInt(document.getElementById('round-result').textContent);
     let targNr = parseInt(document.getElementById('targ-Nr').textContent);
 
-    let gameOver = parseInt(document.getElementById('chances').textContent)
-    gameOver === 0;
-
     let isMatch = result === targNr;
     /*console.log({ isMatch, result, targNr });*/
 
     if (isMatch) {
-        document.getElementById('message').textContent = targNr;
+        document.getElementById('message').textContent = ("A match! You won!");
+        document.getElementById("roll").disabled = true;
+        decrementChances();
         /*alert(`You throw ${targNr}! Congrats it's a match, reset and play again! :D`);*/
     } else if (!isMatch) {
         /*alert(`It's not a match. You have ${chances - 1} left!`);*/
         decrementChances();
-    } else if (gameOver) {
-        alert("You run out chances, reset and play again!");
     } else {
         alert("Hi there!");
     }
@@ -74,187 +86,13 @@ function checkOutcome() {
 
 function decrementChances() {
 
-    chances = parseInt(document.getElementById('chances').innerText);
+    let chances = parseInt(document.getElementById('chances').textContent);
     document.getElementById('chances').innerText = --chances;
 
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-let calculatedSum = document.getElementById('round-result').value;
-let targNr = document.getElementById('targ-Nr').value;
-let chances = document.getElementById('chances').innerText;
-
-let dice = document.querySelectorAll('img');
-/* console.log(dice); */
-
-/*
-
-document.addEventListener('DOMContentLoaded', function () {
-    let buttons = document.getElementsByTagName('button');
-
-    for (let button of buttons) {
-        button.addEventListener('click', function () {
-            if (this.getAttribute('data-type') === 'submit') {
-                /* alert('You roll the dice!'); */
-/*                rollDice();
-                checkOutcome();
-            } else if (this.getAttribute('data-type') === 'reset') {
-                /* alert('GAME OVER - GAME RESET!'); */
-/*                window.location.reload(true);
-            } else {
-                let targNr = this.getAttribute('data-choice');
-                /* console.log(targNr); */
-                /* alert(`You picked the target number ${targNr}!`); */
-/*                document.getElementById("targ-Nr").innerHTML = `You choosen number  ${targNr}! Let's play :)`;
-                document.getElementById("roll").disabled = false;
-            }
-        });
-    }
-    /*rollDice('6');*/
-
-/* });
-
-/**
- * 
- */
-/*function rollDice() {
-    dice.forEach(function (die) {
-        die.classList.add("shake");
-    });
-    setTimeout(function () {
-            dice.forEach(function (die) {
-                die.classList.remove("shake");
-            });
-
-            let dieOneValue = Math.floor(Math.random() * 6) + 1;
-            let firstDiceImage = "assets/images/dice" + dieOneValue + ".png";
-            let dieTwoValue = Math.floor(Math.random() * 6) + 1;
-            let secondDiceImage = "assets/images/dice" + dieTwoValue + ".png";
-            /*console.log(dieOneValue, dieTwoValue);*/
-
-/*            document.querySelector("#die1").setAttribute("src", firstDiceImage);
-            document.querySelector("#die2").setAttribute("src", secondDiceImage);
-            document.querySelector("#round-result").innerHTML = "Your thrown " + ((dieOneValue) + (dieTwoValue));
-            /*result = document.getElementById('round-result').value;*/
-
-/*        },
-        1000
-    );
-}
-
-
-/**
- * Gets the outcome of the throw 
- */
-/*function checkOutcome() {
-
-/*    let isMatch = targNr === calculatedSum;
-    let isNotMatch = targNr !== calculatedSum;
-    let endGame === chances[0];
-
-    if (isMatch) {
-        document.getElementById("message").innerHTML = "You Won!!";
-        alert(`You throw ${targNr}! Congrats it's a match, reset and play again! :D`);
-    } else if (isNotMatch); {
-        document.getElementById("message").innerHTML = "Roll again!!";
-        alert(`It's not a match. You have ${chances - 1} left!`);
-        decrementChances()
-    } else if (endGame); {
-        document.getElementById("message").innerHTML = "You RunOut chances!";
-        alert('Try again!');
+    if (chances === 0) {
+        document.getElementById("roll").disabled = true;
+        document.getElementById('message').textContent = ('Game Over!');
     } else {
-        alert('Hello there!')
+        /*alert('roll again!')*/
     }
 }
-*/
-
-/*
-
-let result = parseInt(document.getElementById('round-result').value);
-    /*console.log('userThrow');*/
-/*
-let isMatch = result === targNr;
-let isNotMatch = result !== targNr;
-
-if (isMatch) {
-    alert(`You throw ${targNr}! Congrats it's a match, reset and play again! :D`);
-} else if (isNotMatch) {
-    /*alert(`It's not a match. You have ${chances - 1} left!`);*/
-/*decrementChances();
-    } else {
-        (chances = 0);
-        alert("You run out chances, reset and play again!");
-    }
-
-/*rollDice();
-
-
-
-/**
- * Gets the total number of throws until the game ends
- */
-/*
-function decrementChances() {
-
-    chances = parseInt(document.getElementById('chances').innerText);
-    document.getElementById('chances').innerText = --chances;
-
-}
-
-
-/*
-let images = ['dice1.png',
-    'dice2.png',
-    'dice3.png',
-    'dice4.png',
-    'dice5.png',
-    'dice6.png'
-];
-
-let images = ["dice-01.svg",
-"dice-02.svg",
-"dice-03.svg",
-"dice-04.svg",
-"dice-05.svg",
-"dice-06.svg"];
-let dice = document.querySelectorAll("img");
-
-function roll(){
-    dice.forEach(function(die){
-        die.classList.add("shake");
-    });
-    setTimeout(function(){
-        dice.forEach(function(die){
-            die.classList.remove("shake");
-        });
-        let dieOneValue = Math.floor(Math.random()*6);
-        let dieTwoValue = Math.floor(Math.random()*6);
-        console.log(dieOneValue,dieTwoValue);
-        document.querySelector("#die-1").setAttribute("src", images[dieOneValue]);
-        document.querySelector("#die-2").setAttribute("src", images[dieTwoValue]);
-        document.querySelector("#total").innerHTML = "Your roll is " + ( (dieOneValue +1) + (dieTwoValue + 1) );
-    },
-    1000
-    );
-}
-roll();
-*/
-
-/*
-// Get the rand.nr dice1 - dice6
-let dieOneValue = Math.floor(Math.random()*6) +1;
-let firstDiceImage = "./assets/images/dice" + dieOneValue + ".png";
-*/
